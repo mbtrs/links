2186b05bd41c8e26698ee7f8af867e9730460063
mb <40812568+mbtrs@users.noreply.github.com>
2023-11-15 22:56:07 -0800

    Update extract-urls.js

--- extract-urls.js
+++ extract-urls.js
@@ -1,15 +1,1 @@
 const fs = require('fs');
-
-const markdownFiles = ['blogs.md', 'cheatsheets.md']; // Add your Markdown files here
-
-const extractedUrls = markdownFiles.reduce((urls, file) => {
-  const content = fs.readFileSync(file, 'utf-8');
-  const matches = content.match(/{%\s*embed\s*url="([^"]+)"\s*%}/g);
-  if (matches) {
-    const fileUrls = matches.map(match => match.match(/"([^"]+)"/)[1]);
-    return urls.concat(fileUrls);
-  }
-  return urls;
-}, []);
-
-fs.writeFileSync('extracted-links.txt', extractedUrls.join('\n'));
